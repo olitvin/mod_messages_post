d11cfff868d186263264f7fb011caebcf334fba7
root <root@ip-172-31-35-117.us-west-2.compute.internal>
2017-06-21 19:58:02 +0000

    last mega code

--- src/mod_messages_post.erl
+++ src/mod_messages_post.erl
@@ -33,8 +33,7 @@
 -export([start/2,
 	 init/2,
 	 stop/1,
-	 send_notice/3,
-     send_packet/4]).
+	 send_notice/3]).
 
 -define(PROCNAME, ?MODULE).
 
@@ -50,15 +49,13 @@ start(Host, Opts) ->
 
 init(Host, _Opts) ->
     inets:start(),
-    ssl:start(),
-    ejabberd_hooks:add(user_send_packet, Host, ?MODULE, send_notice, 10),
-%%%    ejabberd_hooks:add(offline_message_hook, Host, ?MODULE, send_notice, 50),
+    ssl:start(),  
+    ejabberd_hooks:add(message_hook, Host, ?MODULE, send_notice, 10),
     ok.
 
 stop(Host) ->
     ?INFO_MSG("Stopping mod_messages_post", [] ),
-    ejabberd_hooks:delete(user_send_packet, Host, ?MODULE, send_notice, 10),
-%%%    ejabberd_hooks:delete(offline_message_hook, Host, ?MODULE, send_notice, 50),
+    ejabberd_hooks:delete(message_hook, Host, ?MODULE, send_notice, 10),
     ok.
 
 send_notice(From, To, Packet) ->
@@ -66,25 +63,6 @@ send_notice(From, To, Packet) ->
     Body = fxml:get_path_s(Packet, [{elem, <<"body">>}, cdata]),
     Token = gen_mod:get_module_opt(To#jid.lserver, ?MODULE, auth_token, fun(S) -> iolist_to_binary(S) end, list_to_binary("")),
     PostUrl = gen_mod:get_module_opt(To#jid.lserver, ?MODULE, post_url, fun(S) -> iolist_to_binary(S) end, list_to_binary("")),
-
-    if (Type == <<"chat">>) and (Body /= <<"">>) ->
-	      Sep = "&",
-        Post = [
-          "to=", To#jid.luser, Sep,
-          "from=", From#jid.luser, Sep,
-          "body=", url_encode(binary_to_list(Body)), Sep,
-          "access_token=", Token],
-        httpc:request(post, {binary_to_list(PostUrl), [], "application/x-www-form-urlencoded", list_to_binary(Post)},[],[]),
-        ok;
-      true ->
-        ok
-    end.
-
-send_packet(Packet, _C2SState, From, To) ->
-    Type = fxml:get_tag_attr_s(<<"type">>, Packet),
-    Body = fxml:get_path_s(Packet, [{elem, <<"body">>}, cdata]),
-    Token = gen_mod:get_module_opt(From#jid.lserver, ?MODULE, auth_token, fun(S) -> iolist_to_binary(S) end, list_to_binary("")),
-    PostUrl = gen_mod:get_module_opt(From#jid.lserver, ?MODULE, post_url, fun(S) -> iolist_to_binary(S) end, list_to_binary("")),
 
     if (Type == <<"chat">>) and (Body /= <<"">>) ->
 	      Sep = "&",
@@ -140,4 +118,5 @@ old_integer_to_hex(I) when I < 16 ->
 old_integer_to_hex(I) when I >= 16 ->
     N = trunc(I/16),
     old_integer_to_hex(N) ++ old_integer_to_hex(I rem 16).
+
 
